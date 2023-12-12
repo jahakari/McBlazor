3f63687384ee913b7397e8933215ff4ad4e5aa46
jakeh <jakeh@SCREECH-PC>
2023-12-11 17:54:54 -0900

    Implement textarea component.

--- McBlazor/Client/Components/FormTextArea.razor.cs
+++ McBlazor/Client/Components/FormTextArea.razor.cs
@@ -2,8 +2,26 @@
 
 namespace McBlazor.Client.Components;
 
-public partial class FormTextArea : ComponentBase
+public partial class FormTextArea : FormEditorBase<string?>
 {
+    private Func<ChangeEventArgs, Task>? onChangeDelegate;
+    private Func<ChangeEventArgs, Task>? onInputDelegate;
+
+    [Parameter]
+    public bool BindValueOnInput { get; set; }
+
     [Parameter]
     public int Rows { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (BindValueOnInput) {
+            onInputDelegate = EditorChangedAsync;
+        }
+        else {
+            onChangeDelegate = EditorChangedAsync;
+        }
+
+        base.OnInitialized();
+    }
 }
